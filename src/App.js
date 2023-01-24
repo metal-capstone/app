d9fbe78b666eb4b04912ddfb6424aa7e408ed777
Mitchell Sayre <mitchell@vectorone.com>
2023-01-24 00:10:40 -0500

    Add sample request for api (which is missing CORS)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+
+import React, { useState, useEffect } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({message: "no data"});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://0.0.0.0:8000/hello-world");
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,11 +29,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://0.0.0.0:8000/hello-world"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Received {data.message} from our REST API.
         </a>
       </header>
     </div>
