33666da7159ba6bbf48f51198d7b04e93e259b30
loganfrey49 <logantfrey@gmail.com>
2023-02-23 18:43:54 -0500

    Added textbox to send messages and text for server messages

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,38 +1,68 @@
-import React, { useEffect, useState } from "react";
-import useWebSocket from 'react-use-websocket';
+import React, { useEffect, useState, useCallback } from "react";
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import axios from 'axios';
 
 import Header from './Header';
-
 import './Dashboard.css';
+const socketUrl = 'ws://localhost:8000/ws';
 
 function Dashboard() {
-    const [username, setUsername] = useState("No username");
-    const [profilePic, setProfilePic] = useState("no url");
+  const [username, setUsername] = useState();
+  const [profilePic, setProfilePic] = useState();
+  const [currentMessage, setCurrentMessage] = useState('');
+  const [messageHistory, setMessageHistory] = useState([]);
 
-    useWebSocket('ws://localhost:8000/ws', {
-      onOpen: () => {
-        console.log('WebSocket connection established.');
-      }
-    });
+  const { sendMessage, lastMessage, lastJsonMessage, readyState } = useWebSocket(socketUrl, {
+    onOpen: () => {
+      console.log('WebSocket connection established.');
+      axios.get('http://localhost:8000/user-info')
+      .then(function (response) {
+        setUsername(response.data.username);
+        setProfilePic(response.data.profile_pic);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+  });
 
-    // Get user info when rendered
-    useEffect(() => {
-        axios.get('http://localhost:8000/user-info')
-        .then(function (response) {
-          setUsername(response.data.username);
-          setProfilePic(response.data.profile_pic);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }, []);
-  
-    return (
-      <div className="Dashboard">
-        <Header username={username} profilePic={profilePic} />
-      </div>
-    );
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage));
+    }
+  }, [lastMessage, setMessageHistory]);
+
+
+  const handleSend = () => {
+    console.log(currentMessage);
+    sendMessage(currentMessage);
+    setCurrentMessage('');
   }
-  
-  export default Dashboard;
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
+  return (
+    <div className="Dashboard">
+      <Header username={username} profilePic={profilePic} />
+      <p>{connectionStatus}</p>
+      <label>
+        Message:
+        <input type="text" value={currentMessage} onChange={message => { setCurrentMessage(message.target.value) }} />
+      </label>
+      <button onClick={handleSend}>Send</button>
+      <ul>
+        {messageHistory.map((message, idx) => (
+          <span key={idx}>{message ? message.data : null}</span>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Dashboard;
