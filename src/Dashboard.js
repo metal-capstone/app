c4fd4eb0a2d61ade48b39c2461bb842069491ed2
loganfrey49 <logantfrey@gmail.com>
2023-02-23 14:57:57 -0500

    added simple websocket connection on dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import useWebSocket from 'react-use-websocket';
 import axios from 'axios';
 
 import './Dashboard.css';
@@ -6,6 +7,12 @@ import './Dashboard.css';
 function Dashboard() {
     const [username, setUsername] = useState("No username");
     const [profilePic, setProfilePic] = useState("no url");
+
+    useWebSocket('ws://localhost:8000/ws', {
+      onOpen: () => {
+        console.log('WebSocket connection established.');
+      }
+    });
 
     // Get user info when rendered
     useEffect(() => {
