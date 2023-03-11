ca3aaef2b11385e43b82d0e463962000078cb2f1
loganfrey49 <logantfrey@gmail.com>
2023-03-11 02:14:30 -0500

    Added buttons to player, and live update every second

--- src/WebPlayer.js
+++ src/WebPlayer.js
@@ -4,31 +4,41 @@ import axios from 'axios';
 import './WebPlayer.css';
 
 function WebPlayer({ token }) {
-    const [isPaused, setPaused] = useState(false);
+    const [isPlaying, setPlaying] = useState(false);
     const [isActive, setActive] = useState(false);
     const [currentTrack, setTrack] = useState('No Song Playing');
     const [currentArtist, setArtist] = useState('No Artist');
     const [currentAlbumArt, setAlbumArt] = useState();
 
     useEffect(() => {
-        if (token !== undefined) {
-            const config = {
-                headers: {
-                    'Authorization': 'Bearer ' + token,
-                    'Content-Type': 'application/json'
-                }
-            };
-            axios.get('https://api.spotify.com/v1/me/player', config).then(function (response) {
-                setTrack(response.data.item.name);
-                setArtist(response.data.item.artists[0].name);
-                setAlbumArt(response.data.item.album.images[0].url);
-                setActive(true);
-            }).catch(function (error) {
-                console.log(error);
-            });
-        } else {
-            setActive(false);
-        }
+        const updatePlayback = async () => {
+            if (token !== undefined) {
+                const config = {
+                    headers: {
+                        'Authorization': 'Bearer ' + token,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                axios.get('https://api.spotify.com/v1/me/player/currently-playing', config).then(function (response) {
+                    if (response.status == 200) {
+                        setTrack(response.data.item.name);
+                        setArtist(response.data.item.artists[0].name);
+                        setAlbumArt(response.data.item.album.images[0].url);
+                        setPlaying(response.data.is_playing);
+                        setActive(true);
+                    } else {
+                        setActive(false);
+                    }
+                }).catch(function (error) {
+                    console.log(error);
+                    setActive(false);
+                });
+            } else {
+                setActive(false);
+            }
+        };
+        const interval = setInterval(() => { updatePlayback(); }, 1000);
+        return () => clearInterval(interval);
     }, [token]);
 
     return (
@@ -37,7 +47,11 @@ function WebPlayer({ token }) {
             <div>
                 <div>{currentTrack}</div>
                 <div>{currentArtist}</div>
-                <div className={`${isActive ? 'ControlButtons' : 'ControlButtonsLight'}`}><div>Back</div><div>Play</div><div>Forward</div></div>
+                <div className={`${isActive ? 'ControlButtons' : 'ControlButtonsLight'}`}>
+                    <img src='/previous-button.svg'></img>
+                    <img src={`${isPlaying ? '/pause-button.svg' : '/play-button.svg'}`}></img>
+                    <img src='/next-button.svg'></img>
+                </div>
                 {!isActive && <div>Playback is not active. Start playing music on Spotify to start.</div>}
             </div>
         </div>
