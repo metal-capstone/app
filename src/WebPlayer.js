85f537b1a9e3bab8a7f9b6ce972bec76372ea4e8
loganfrey49 <logantfrey@gmail.com>
2023-03-21 15:15:54 -0400

    Finished basic webplayer with playback control and info

--- src/WebPlayer.js
+++ src/WebPlayer.js
@@ -6,8 +6,8 @@ import './WebPlayer.css';
 function WebPlayer({ token }) {
     const [isPlaying, setPlaying] = useState(false);
     const [isActive, setActive] = useState(false);
-    const [currentTrack, setTrack] = useState('No Song Playing');
-    const [currentArtist, setArtist] = useState('No Artist');
+    const [currentTrack, setTrack] = useState();
+    const [currentArtist, setArtist] = useState();
     const [currentAlbumArt, setAlbumArt] = useState();
     const [currentTrackLength, setTrackLength] = useState(0);
     const [currentTrackProgress, setTrackProgress] = useState(0);
@@ -22,7 +22,7 @@ function WebPlayer({ token }) {
                     }
                 };
                 axios.get('https://api.spotify.com/v1/me/player/currently-playing', config).then(function (response) {
-                    if (response.status == 200) {
+                    if (response.status === 200) {
                         setTrack(response.data.item.name);
                         setArtist(response.data.item.artists[0].name);
                         setAlbumArt(response.data.item.album.images[0].url);
@@ -45,23 +45,65 @@ function WebPlayer({ token }) {
         return () => clearInterval(interval);
     }, [token]);
 
+    const playPauseSong = async () => {
+        const config = {
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            }
+        };
+        if (isPlaying) {
+            axios.put('https://api.spotify.com/v1/me/player/pause', {}, config).catch(function (error) {
+                console.log('Error pausing song: ' + error);
+            });
+        } else {
+            axios.put('https://api.spotify.com/v1/me/player/play', {}, config).catch(function (error) {
+                console.log('Error playing song: ' + error);
+            });
+        }
+    }
+
+    const previousSong = async () => {
+        const config = {
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            }
+        };
+        axios.post('https://api.spotify.com/v1/me/player/previous', {}, config).catch(function (error) {
+            console.log('Error previous song: ' + error);
+        });
+    }
+
+    const nextSong = async () => {
+        const config = {
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            }
+        };
+        axios.post('https://api.spotify.com/v1/me/player/next', {}, config).catch(function (error) {
+            console.log('Error next song: ' + error);
+        });
+    }
+
     const msToMinSec = (ms) => {
-        var mins = Math.floor(ms / 60000);
-        var secs = ((ms % 60000) / 1000).toFixed(0);
-        return mins + ":" + (secs < 10 ? '0' : '') + secs;
+        const secs = Math.floor((ms / 1000) % 60);
+        const mins = Math.floor((ms / 1000 / 60) % 60);
+        return (secs === 60) ? (mins + 1) + ":00" : mins + ":" + (secs < 10 ? "0" : "") + secs;
     };
 
     return (
         <div className='WebPlayer'>
-            <img className='AlbumArt' src={currentAlbumArt || '/blank-album-art.png'}></img>
+            <img className='AlbumArt' src={currentAlbumArt || '/blank-album-art.png'} alt='Album Art'></img>
             <div>
-                <div>{currentTrack}</div>
-                <div>{currentArtist}</div>
+                <div>{currentTrack || 'No Song Playing'}</div>
+                <div>{currentArtist || 'No Artist'}</div>
                 <div>{msToMinSec(currentTrackProgress)}/{msToMinSec(currentTrackLength)}</div>
                 <div className={`${isActive ? 'ControlButtons' : 'ControlButtonsLight'}`}>
-                    <img src='/previous-button.svg'></img>
-                    <img src={`${isPlaying ? '/pause-button.svg' : '/play-button.svg'}`}></img>
-                    <img src='/next-button.svg'></img>
+                    <img src='/previous-button.svg' alt='Previous Song' onClick={previousSong}></img>
+                    <img src={`${isPlaying ? '/pause-button.svg' : '/play-button.svg'}`} alt='Play or Pause' onClick={playPauseSong}></img>
+                    <img src='/next-button.svg' alt='Next Song' onClick={nextSong}></img>
                 </div>
                 {!isActive && <div>Playback is not active. Start playing music on Spotify to start.</div>}
             </div>
